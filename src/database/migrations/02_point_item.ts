d78f3e79b280628337c41eab63cba321ddac337e
Fernando Teixeira <fernando.teixeira@goldengatetec.com.br>
2020-06-14 12:03:48 -0300

    feat: initial configuration and migrations

---
+++ src/database/migrations/02_point_item.ts
@@ -0,0 +1,12 @@
+import Knex from 'knex';
+
+export async function up (knex: Knex) {
+  return knex.schema.createTable('point_item', table => {
+    table.integer('point_id').notNullable().references('id').inTable('point');
+    table.integer('item_id').notNullable().references('id').inTable('item');
+  });
+};
+
+export async function down (knex: Knex) {
+  return knex.schema.dropSchema('point_item');
+}; 
